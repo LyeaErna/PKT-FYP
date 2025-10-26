6a14b3748286cc63512d7e7158987fc207ac2f87
Lyea <emyliaerna05@gmail>
2025-10-27 03:14:55 +0800

    Fix Typo

--- src/login/Register.js
+++ src/login/Register.js
@@ -30,7 +30,7 @@ export default function RegisterForm() {
     setSuccess("");
 
     try {
-      const response = await fetch("htts://okutransport.site/backend/register.php", {
+      const response = await fetch("https://okutransport.site/backend/register.php", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, userType, phone, email, password }),
